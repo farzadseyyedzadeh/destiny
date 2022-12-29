c5938f5678dac22adcf0581d14ddcb65cce49489
farzad seyyedzadeh <85734789+farzadseyyedzadeh@users.noreply.github.com>
2022-12-29 20:38:03 +0330

    Update story.dart

--- lib/story.dart
+++ lib/story.dart
@@ -2,6 +2,7 @@ class Story {
   String storyTitle;
   String choice1;
   String choice2;
+  String choice3;
 
   Story(
       {required this.storyTitle, required this.choice1, required this.choice2});
